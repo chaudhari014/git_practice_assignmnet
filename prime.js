df15416c8f99acd455ea1beda4e39dfed7a01a41
kishan1956 <fataram.k14@gmail.com>
2022-12-04 16:25:59 +0530

    yes

--- prime.js
+++ prime.js
@@ -1,4 +1,4 @@
-function primenumber(n){
+  let n=3
   let count=0;
   for(let i=1;i<=n;i++){
     if(n%i==0){
@@ -11,4 +11,3 @@ function primenumber(n){
   else{
     console.log(n,"number is not prime")
   }
-}
